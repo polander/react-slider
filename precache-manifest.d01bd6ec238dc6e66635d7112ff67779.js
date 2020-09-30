dd3cce0c85115648d1ede364ebaa899cba369a2c
Dmitry <polander96@gmail.com>
2020-09-30 16:56:32 +0300

    Updates

---
+++ precache-manifest.d01bd6ec238dc6e66635d7112ff67779.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe7392e919d8cc1f5af9d29ed6fbbd2",
+    "url": "/react-slider/index.html"
+  },
+  {
+    "revision": "eea4c105bc0dd5b784bb",
+    "url": "/react-slider/static/css/main.0ef32b05.chunk.css"
+  },
+  {
+    "revision": "605a5c291993af22521c",
+    "url": "/react-slider/static/js/2.8b618a85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-slider/static/js/2.8b618a85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eea4c105bc0dd5b784bb",
+    "url": "/react-slider/static/js/main.a1b119bb.chunk.js"
+  },
+  {
+    "revision": "14182e5e6d736833f2dc",
+    "url": "/react-slider/static/js/runtime-main.4b11de5c.js"
+  },
+  {
+    "revision": "6867dc1d8c3f290c147c21b885e73fd2",
+    "url": "/react-slider/static/media/left-arrow.6867dc1d.svg"
+  },
+  {
+    "revision": "445abfc59ed4db25bb9f34a6bab8ca88",
+    "url": "/react-slider/static/media/right-arrow.445abfc5.svg"
+  }
+]);
